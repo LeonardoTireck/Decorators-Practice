26d290d201b09c28511a6b31670d37dbe4df3133
Leonardo Tireck <leonardotireck@Leonardos-MacBook-Air.local>
2025-06-17 09:20:08 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,74 @@
+// ES DECORATOR EXAMPLE
+// ) {
+//   context.addInitializer(function (this: any) {
+//     if (context.private)
+//       throw new TypeError("Not supported on private methods.");
+//     this[context.name] = this[context.name].bind(this);
+//   });
+// }
+
+// Experimental Decorator Example
+function autobind(
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor,
+) {
+  const originalDescriptor = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      return originalDescriptor.bind(this);
+    },
+  };
+  return adjustedDescriptor;
+}
+
+class ProjectInput {
+  public templateElement: HTMLTemplateElement;
+  public hostElement: HTMLDivElement;
+  public element: HTMLFormElement;
+  public titleEl: HTMLInputElement;
+  public descriptionEl: HTMLInputElement;
+  public peopleEl: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input",
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true,
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+
+    this.element.id = "user-input";
+
+    this.titleEl = this.element.querySelector("#title")! as HTMLInputElement;
+    this.descriptionEl = this.element.querySelector(
+      "#description",
+    )! as HTMLInputElement;
+    this.peopleEl = this.element.querySelector("#people")! as HTMLInputElement;
+
+    this.configure();
+
+    this.attach();
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleEl.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
